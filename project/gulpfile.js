c1249c13e0ead19443f77b20e697e080f165a03c
Luis Carlos Chavarría <luis.carlos@meltwater.com>
2015-09-12 09:53:08 -0600

    Add sass first class.

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -4,9 +4,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('./sass/**/*.scss')
+    gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('sass:watch', function () {
