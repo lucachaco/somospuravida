4b360bbdbd02bfc3a11f9dd042c11eb987f23fa6
Luis Carlos Chavarría <luis.carlos@meltwater.com>
2015-09-11 18:16:22 -0600

    Update con.

--- conf/bootstrap.sh
+++ conf/bootstrap.sh
@@ -9,6 +9,10 @@ locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
 apt-get install -y git
+apt-get install -y build-essential
 
-apt-get install -y ruby ruby-dev make gcc nodejs
-
+#install node
+apt-get install -y python-software-properties
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
